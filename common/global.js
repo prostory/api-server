418db415d5614db879fbf419e4163d2093aefcd9
perry <13410365214@163.com>
2019-12-20 15:17:55 +0800

    support https

--- common/global.js
+++ common/global.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 module.exports = {
     env: env,
     base_url: env === 'development' ? 'http://127.0.0.1:3000/api/'
-        : 'http://106.54.193.117/api/',
+        : 'https://littlethings.cn/api/',
     admin: {
         username: 'Condor',
         password: 'condordz'
