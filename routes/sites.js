cc34fc773b4de4b0364e11d713a971a297147bfa
perry <13410365214@163.com>
2020-02-26 08:15:26 +0800

    public site fetch api

--- routes/sites.js
+++ routes/sites.js
@@ -14,32 +14,30 @@ router.get("/:brand", function(req, res, next) {
 });
 
 router.post("/fetch", function(req, res, next) {
-  jwt.verifyLogin(req, res, function() {
-    var filter = req.body.filter || {};
-    Object.keys(filter).forEach(key => {
-      if (typeof filter[key] === "string" && filter[key].indexOf("/") === 0) {
-        filter[key] = eval(filter[key]);
-      }
-    });
-    Site.countDocuments(filter, function(err, count) {
-      if (err) {
-        res.error("Query failed", err);
-      } else {
-        var skip = req.body.skip || 0;
-        var limit = req.body.limit || 10;
-        var sort = req.body.sort || { created_at: -1 };
-        Site.fetch(filter, skip, limit, sort, function(err, result) {
-          if (err) {
-            res.error("Query failed", err);
-          } else {
-            res.return("Query successfully", {
-              result: result,
-              count: count
-            });
-          }
-        });
-      }
-    });
+  var filter = req.body.filter || {};
+  Object.keys(filter).forEach(key => {
+    if (typeof filter[key] === "string" && filter[key].indexOf("/") === 0) {
+      filter[key] = eval(filter[key]);
+    }
+  });
+  Site.countDocuments(filter, function(err, count) {
+    if (err) {
+      res.error("Query failed", err);
+    } else {
+      var skip = req.body.skip || 0;
+      var limit = req.body.limit || 10;
+      var sort = req.body.sort || { created_at: -1 };
+      Site.fetch(filter, skip, limit, sort, function(err, result) {
+        if (err) {
+          res.error("Query failed", err);
+        } else {
+          res.return("Query successfully", {
+            result: result,
+            count: count
+          });
+        }
+      });
+    }
   });
 });
 
